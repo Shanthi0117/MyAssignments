63c593cec33e6f9b9734df8e9e7a56e28fbff25b
Shanthi0117 <krshanthi2303@gmail.com>
2026-02-05 23:11:49 +0530

    Add files via upload
    HomeAssignment_EmployeeDetails

---
+++ package1/EmployeeDetails.java
@@ -0,0 +1,28 @@
+package package1;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name="John Doe";
+		int age=30;
+		float salary=55000.75f;
+		char gender='M';
+		boolean acceptTerms=true;
+		float rating=4.5f;
+		long mobileNumber=9876543210l;
+		int pincode=56001;
+		int luckyNumber=7;
+		
+		System.out.println("Name:" +name);
+		System.out.println("Age:" +age);
+		System.out.println("Salary:" +salary);
+		System.out.println("Gender:" +gender);
+		System.out.println("AcceptTerms:" +acceptTerms);
+		System.out.println("Rating:" +rating);
+		System.out.println("MobileNumber:" +mobileNumber);
+		System.out.println("Pincode:" +pincode);
+		System.out.println("LuckyNumber:" +luckyNumber);
+}
+
+}
